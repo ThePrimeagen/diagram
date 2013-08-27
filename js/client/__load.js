d9b25db45e7a0a2f8204830d9e5b08a0f44f41ce
Shane Sizer <shane.sizer@webfilings.com>
2013-08-26 23:51:06 -0600

    Adding console statements, and some trivial changes to play around with meteor and understand current state of project.

--- js/client/__load.js
+++ js/client/__load.js
@@ -2,5 +2,6 @@ require([
     'js/client/data/data',
     'js/client/main'
 ], function(Data, Main) {
+    console.log('__load');
     // TODO: Make loading point
 });
--- js/common/__load.js
+++ js/common/__load.js
@@ -1,6 +1,7 @@
+// No reason for common to exist separate from lib right?
 require([
     'js/common/data',
     'js/common/methods'
 ], function() {
-
+    console.log('js/common/__load');
 });
